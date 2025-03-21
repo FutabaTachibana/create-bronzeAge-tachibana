f5addd7da9c4348872851afa307343f126df2425
Futaba_Tachibana <futabatachibana@icloud.com>
2025-03-21 23:34:08 +0800

    added textures of those items, and changed some codes.

--- src/main/java/org/syju/bronze_age_tachibana/Tags.java
+++ src/main/java/org/syju/bronze_age_tachibana/Tags.java
@@ -1,6 +1,7 @@
 package org.syju.bronze_age_tachibana;
 
 import com.tterrag.registrate.util.entry.ItemEntry;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
@@ -21,25 +22,38 @@ public class Tags {
     }
 
     // Helper methods to add tags to items & blocks & fluid
-    public static <T> TagKey<T> optionalTag(IForgeRegistry<T> registry, ResourceLocation id) {
-        return registry.tags()
-                .createOptionalTagKey(id, Collections.emptySet());
-    }
+//    public static <T> TagKey<T> optionalTag(IForgeRegistry<T> registry, ResourceLocation id) {
+//        return registry.tags()
+//                .createOptionalTagKey(id, Collections.emptySet());
+//    }
 
-    public static <T> TagKey<T> forgeTag(IForgeRegistry<T> registry, String path) {
-        return optionalTag(registry, ResourceLocation.fromNamespaceAndPath("forge", path));
-    }
+//    public static <T> TagKey<T> forgeTag(IForgeRegistry<T> registry, String path) {
+//        return optionalTag(registry, ResourceLocation.fromNamespaceAndPath("forge", path));
+//    }
+//
+//    public static TagKey<Block> forgeBlockTag(String path) {
+//        return forgeTag(ForgeRegistries.BLOCKS, path);
+//    }
+//
+//    public static TagKey<Item> forgeItemTag(String path) {
+//        return forgeTag(ForgeRegistries.ITEMS, path);
+//    }
+//
+//    public static TagKey<Fluid> forgeFluidTag(String path) {
+//        return forgeTag(ForgeRegistries.FLUIDS, path);
+//    }
 
+    // Helper methods to add tags to items & blocks & fluid
     public static TagKey<Block> forgeBlockTag(String path) {
-        return forgeTag(ForgeRegistries.BLOCKS, path);
+        return TagKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath("forge", path));
     }
 
     public static TagKey<Item> forgeItemTag(String path) {
-        return forgeTag(ForgeRegistries.ITEMS, path);
+        return TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath("forge", path));
     }
 
     public static TagKey<Fluid> forgeFluidTag(String path) {
-        return forgeTag(ForgeRegistries.FLUIDS, path);
+        return TagKey.create(Registries.FLUID, ResourceLocation.fromNamespaceAndPath("forge", path));
     }
 }
 
